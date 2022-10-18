1b60a4cae052781822d3766b66e6a3628c6648c8
vovacelidze <louglescom@gmail.com>
2022-10-18 21:29:24 +0300

    student form with old reducer is working

--- src/components/StudentPage.jsx
+++ src/components/StudentPage.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { v4 as uuidv4 } from 'uuid';
 import { ADD_STUDENT_CREATOR } from '../store/studentReducer';
 
 const StudentPage = () => {
-  const [input, setInput] = useState();
   const {students} = useSelector(state => state.student);
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.currentTarget;
-    const input = form.elements.name.value;
+    const input = form.elements.vova.value;
+    console.log(form.elements.vova.value)
     const user = {
       id: uuidv4(),
       name: input
@@ -19,17 +19,12 @@ const StudentPage = () => {
     dispatch(ADD_STUDENT_CREATOR(user));
     form.reset();
   }
-  function AddStudent(name) {
-    const user = {
-      id: uuidv4(),
-      name: name
-    }
-    console.log(user);
-    dispatch(ADD_STUDENT_CREATOR(user));
-  }
-
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='vova'></input>
+        <button type='submit'>Add Student</button>
+      </form>
       {students.length ? 
       students.map(student =>
         <div key={student.id}>{student.name}</div>
@@ -37,11 +32,6 @@ const StudentPage = () => {
       : 
       <h1>There is no students yet</h1>
       }
-      <form onSubmit={handleSubmit}>
-        <input type='text' name='name'></input>
-        <button type='submit'>Add Student</button>
-      </form>
-      {/* <button onClick={() => AddStudent(prompt())}>TEST</button> */}
     </div>
   )
 }
