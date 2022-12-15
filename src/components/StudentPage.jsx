62529a038c4228cca5c18b56ee67719998ecf4c4
vovacelidze <louglescom@gmail.com>
2022-12-15 14:50:47 +0200

    added price value to studentPage

--- src/components/StudentPage.jsx
+++ src/components/StudentPage.jsx
@@ -1,52 +1,77 @@
-import { Button, FormControl } from 'react-bootstrap';
-import React from 'react'
+import { Button, Container, Form, ListGroup, ListGroupItem } from 'react-bootstrap';
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { v4 as uuidv4 } from 'uuid';
-import { fetchManyStudents } from '../asyncFetch/fetchManyStudents';
-import { ADD_STUDENT_CREATOR, DELETE_STUDENT_CREATOR } from '../store/studentReducer';
+// import { fetchManyStudents } from '../asyncFetch/fetchManyStudents';
+import { ADD_MANY_STUDENTS_CREATOR, ADD_STUDENT_CREATOR, DELETE_STUDENT_CREATOR } from '../store/studentReducer';
 
 const StudentPage = () => {
   const {students} = useSelector(state => state.student);
   const dispatch = useDispatch();
 
+  // useEffect(() => {
+  //   const fetchManyStudents = () => {
+  //     return dispatch => {
+  //       try {   
+  //         fetch('https://jsonplaceholder.typicode.com/users')
+  //         .then(response => response.json())
+  //         .then(json => dispatch(ADD_MANY_STUDENTS_CREATOR(json)));
+  //       } catch (e) {
+  //         console.log(e.message);
+  //       }
+  //     }
+  //   }
+  //   dispatch(fetchManyStudents())
+  // }, [])
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.currentTarget;
-    const input = form.elements.vova.value;
+    const inputName = form.elements.name.value;
+    const inputPrice = form.elements.price.value;
     const user = {
       id: uuidv4(),
-      name: input
+      name: inputName,
+      price: inputPrice
     }
-    if(input){
+    if(inputName){
       dispatch(ADD_STUDENT_CREATOR(user));
+      form.reset();
       return
     }
-    console.log(form);
-    form.reset();
   }
-  const handleFetchStudent = () => {
-    dispatch(fetchManyStudents());
-  }
+  // const handleFetchStudent = () => {
+  //   dispatch(fetchManyStudents());
+  // }
   const handleDeleteStudent = (payload) => {
     dispatch(DELETE_STUDENT_CREATOR(payload))
   }
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <FormControl type='text' name='vova'></FormControl>
-        <Button variant='dark' type='submit'>Add Student</Button>
-      </form>
-      <Button variant="dark" onClick={handleFetchStudent}>Fetch Students</Button>
+  <Container className='mt-4' fluid='sm'>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Input name</Form.Label>
+        <Form.Control type="text" placeholder="Enter name" name='name'/>
+        <Form.Label>Input price</Form.Label>
+        <Form.Control type="number" placeholder="Enter price" name='price'/>
+      </Form.Group>
+      <Button variant="dark" type="submit">
+        ADD
+      </Button>
+    </Form>
+    <ListGroup className='mt-4 pt-2'>
       {students.length ? 
-      students.map(student =>
-        <div key={student.id}>{student.name}
+        students.map(student =>
+        <ListGroupItem key={student.id} className='d-flex justify-content-between'>
+          {student.name} | {student.price} UAH
           <Button variant="danger" onClick={() => handleDeleteStudent(student.id)}>X</Button>
-        </div>
-      )
+        </ListGroupItem>
+        )
       : 
-      <h1>There is no students yet</h1>
+        <h1 className='d-flex justify-content-center'>There is no students yet</h1>
       }
-    </div>
+    </ListGroup>
+  </Container>
   )
 }
 
